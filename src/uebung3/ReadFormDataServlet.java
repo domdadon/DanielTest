8340b0a7db174433ba3d7c7c9754967dbb411a0b
dar4554 <39005229+dar4554@users.noreply.github.com>
2018-06-03 13:34:36 +0200

    Bugfix

--- src/uebung3/ReadFormDataServlet.java
+++ src/uebung3/ReadFormDataServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReadFormDataServlet
  */
-@WebServlet("/ReadFormDataServlet")
+@WebServlet("/readformdataservlet")
 public class ReadFormDataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
